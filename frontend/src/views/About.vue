0781ff9188ac4b4c7ae8637966260f9571b021ac
jeybeyen <jeybeyen@gmail.com>
2019-03-07 10:31:26 +0530

    Added backend api

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+	<button class=”Search__button” @click="callRestService()">CALL Spring Boot REST backend service</button>
+	<h3>{{ response }}</h3>
+	<h3>Username : {{username}}</h3>
   </div>
 </template>
+<script>
+	import axios from 'axios'
+
+export default{
+name : 'about',
+data (){
+  return {
+    response: [],
+	username : '',
+    errors: []
+  }
+},
+methods:{
+callRestService (){
+  axios.get(`api/v1/userinfo`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.response = response.data,
+		this.username = response.data.username
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+	}
+}
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js Apps.."/>
+	<button class=”Search__button” @click="callRestService()">CALL Spring Boot REST backend service</button>
+	<h3>{{ response }}</h3>
   </div>
 </template>
 
