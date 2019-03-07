3bf62b30b1fe26c09c8f1aabf86886b1f8734251
jeybeyen <jeybeyen@gmail.com>
2019-03-07 13:49:53 +0530

    Added new dasboard

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,6 @@
-// vue.config.js
 module.exports = {
+  lintOnSave: false,
+  runtimeCompiler: true,
   // proxy all webpack dev-server requests starting with /api
   // to our Spring Boot backend (localhost:8088) using http-proxy-middleware
   // see https://cli.vuejs.org/config/#devserver-proxy
